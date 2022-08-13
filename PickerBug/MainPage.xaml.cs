c673dfbc11a76035ae60b63b796b03040284e351
F-Unction <f_unction@qq.com>
2022-08-13 18:20:25 +0800

    init

--- PickerBug/MainPage.xaml.cs
+++ PickerBug/MainPage.xaml.cs
@@ -7,18 +7,13 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+
+		picker.Items.Add("item");
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		picker.Items.Add("won't appear");
 	}
 }
 
