30da2c9c0f3a66ab6755721a5d865c60c6c10f2a
F-Unction <f_unction@qq.com>
2022-08-13 18:16:11 +0800

    添加项目文件。

---
+++ PickerBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PickerBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PickerBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PickerBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
